3313a9a8a115081c086ff58df21002ec9601ea00
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2022-11-28 20:01:50 -0300

    Bugs in Samples updates

--- Bugs in Samples/Data/TravelAppSelectedArticles.cs
+++
@@ -1,9 +0,0 @@
-//namespace Bugs_in_Samples.Data.TravelApp; // Razor won't recognize third level namespace
-namespace Bugs_in_Samples.TravelApp;
-
-public class SelectedArticlesType
-{
-    public string? Title { get; set; }
-    public string? Subtitle { get; set; }
-    public string? Image_url { get; set; }
-}
